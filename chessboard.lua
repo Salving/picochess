58da6c7b3ce631a7d77dede3fa84e9dcf090ea0f
Salving <Salving222@yandex.ru>
2025-01-30 12:29:14 +0700

    Шахматное поле

---
+++ chessboard.lua
@@ -0,0 +1,52 @@
+﻿isoW = 2
+isoH = 1
+gridStartX = 64
+gridStartY = 64
+tileWidth = 7
+tileHeight = 7
+
+tileColors = { [0] = 13, 15, 3 }
+selectedTile = { x = 0, y = 0 }
+
+field = {}
+
+function initField()
+    for x = 0, 7 do
+        field[x] = {}
+        for y = 0, 7 do
+            field[x][y] = (x + y) % 2
+        end
+    end
+end
+
+function clearField()
+    for x, row in pairs(field) do
+        for y, v in pairs(row) do
+            row[y] = (x + y) % 2;
+        end
+    end
+end
+
+function drawTile(x, y, color)
+    for i = 0, tileWidth do
+        for j = 0, tileHeight do
+            local scX, scY = isoToScreen(x + i, y + j)
+            pset(scX, scY, color)
+            pset(scX - 1, scY, color)
+            pset(scX, scY + 1, color)
+            pset(scX - 1, scY + 1, color)
+        end
+    end
+end
+
+function isoToScreen(x, y)
+    local screenX = gridStartX + (x - y) * isoW
+    local screenY = gridStartY + (x + y) * isoH
+    return screenX, screenY
+end
+
+function screenToIso(scrX, scrY)
+    local x = ((scrY - gridStartY) / isoH + (scrX - gridStartX) / isoW) / 2
+    local y = ((scrY - gridStartY) / isoH - (scrX - gridStartX) / isoW) / 2
+    return flr(x / (tileWidth + 1)), flr(y / (tileHeight + 1))
+end
--- main.lua
+++ main.lua
@@ -1,1 +1,40 @@
-﻿print("test")
+﻿mouseX, mouseY = 0, 0
+cameraOffsetX, cameraOffsetY = 0, 0
+
+function mousePos()
+    return stat(32), stat(33)
+end
+
+function _init()
+    printh("---LOG START---", "log.txt", true)
+    poke(0x5F2D, 0x1)
+    
+    initField()
+end
+
+function _update()
+    clearField()
+
+    local tileX, tileY = screenToIso(mouseX + cameraOffsetX, mouseY + cameraOffsetY)
+    if (tileX >= 0 and tileX < 8 and tileY >= 0 and tileY < 8) then
+        selectedTile = { x = tileX, y = tileY }
+        field[tileX][tileY] = 2
+    end
+end
+
+function _draw()
+    cls(2)
+
+    mouseX, mouseY = mousePos()
+    cameraOffsetX = (mouseX - 64) * 2
+    cameraOffsetY = mouseY
+    camera(cameraOffsetX, cameraOffsetY)
+
+    for gridX, row in pairs(field) do
+        for gridY, v in pairs(row) do
+            drawTile(gridX * 8, gridY * 8, tileColors[v])
+        end
+    end
+
+    spr(0, mouseX + cameraOffsetX, mouseY + cameraOffsetY)
+end
